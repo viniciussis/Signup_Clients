67933eec6d0b71de07c458970577d97f1e4e810b
viniciussis <vinicius01012@gmail.com>
2025-11-07 07:11:32 -0300

    chore: remove NestJS module files, config, and old connections

--- src/infrastructure/http/http.module.ts
+++
@@ -1,9 +0,0 @@
-import { ApplicationModule } from '@/application/application.module';
-import { ClienteController } from './controllers/cliente.controller';
-import { Module } from '@nestjs/common';
-
-@Module({
-  imports: [ApplicationModule],
-  controllers: [ClienteController],
-})
-export class HttpModule {}
--- src/infrastructure/messaging/constants.ts
+++
@@ -1,1 +0,0 @@
-export const RABBITMQ_SERVICE = Symbol('RABBITMQ_SERVICE');
